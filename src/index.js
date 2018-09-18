374727d4194ef1b41ae63066f3c5f7823b0823c5
Yan Makouski <makovskiy.yan@gmail.com>
2018-09-18 19:10:22 +0300

    solution for counting 0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var zeros = 0;
+  var i = 1;  
+  while (Math.floor(number / Math.pow(5, i)) > 0) {
+    zeros += Math.floor(number / Math.pow(5, i))
+    i++;
+  }
+  return zeros;
 }
